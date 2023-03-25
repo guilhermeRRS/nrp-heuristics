f3793b10168577ca62426ecd42aad81d55219824
Guilherme Ramon Rodrigues da Silva <gramon.silva@hotmail.com>
2023-03-24 23:42:02 -0300

    Solver done

---
+++ job/create.py
@@ -0,0 +1,17 @@
+import json, io, sys, os
+
+description = "solver"
+time = 3600
+
+description = f"description_{time}"
+
+for i in range(1,25):
+	instance = str(i)
+	fileName = f"{instance}_{description}.job"
+	content = f"#PBS -N {instance}_{description}\n#PBS -l select=1:ncpus=1:nodetype=n40\n#PBS -l walltime=240:00:00\n\nmodule load python/3.6.8-gurobi\nmodule load gurobi/9.0.1\npython main.py "
+	content += f"{instance} {time} {description}"
+	try:
+		arquivo = io.open(fileName, "w+", encoding = "utf8")
+		arquivo.write(content)
+	except:
+		print("Erro",i)
--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 # coding=utf-8
 import logging
+import sys
 from model import NurseModel
 from solver import Solver
 from chronos import Chronos
 
-PATH_DATA = "../instancias/"
-PATH_MODEL = "../modelos/"
-PATH_SOLUTION = "../initial/"
+PATH_DATA = "instancias/"
+PATH_MODEL = "modelos/"
+#PATH_GET_SOLUTION = "initial/"
+PATH_SAVE_SOLUTION = "o_solutions/"
+PAT_LOG = "o_logs/"
 
-instance = "10"
+instance = str(int((sys.argv[1:])[0]))
+timeLimit = int((sys.argv[1:])[1])
+description = str(((sys.argv[1:])[2]))
 
-logging.basicConfig(level=logging.DEBUG, filename=f'{instance}.log', filemode='w', format='%(message)s')
+logging.basicConfig(level=logging.DEBUG, filename=f'{PAT_LOG}{instance}_{description}.log', filemode='w', format='%(message)s')
 logging.getLogger("gurobipy.gurobipy").disabled = True
 
 nurse = NurseModel()
 nurse.setPathData(f"{PATH_DATA}Instance{instance}.txt")
 nurse.setPathModel(f"{PATH_MODEL}modelo{instance}.lp")
-nurse.setPathSolution(f"{PATH_SOLUTION}{instance}.sol")
+#nurse.setPathSolution(f"{PATH_GET_SOLUTION}{instance}.sol")
 
 nurse.getData()
 nurse.getModel()
 #nurse.getSolution()
 
-chronos = Chronos(5)
+chronos = Chronos(timeLimit = timeLimit)
 
 solver = Solver(nurseModel = nurse, chronos = chronos)
 success, nurse = solver.run()
 
 print(success)
-print(nurse.solution.printSolution(f"{instance}.sol", nurse.data.sets))
+print(nurse.solution.printSolution(f"{PATH_SAVE_SOLUTION}{instance}_{description}.sol", nurse.data.sets))
 print(chronos.done())
