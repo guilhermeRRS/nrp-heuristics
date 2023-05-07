0fb5e2c89db60360b5cea93ab6e01be5cf756638
Guilherme Ramon Rodrigues da Silva <gramon.silva@hotmail.com>
2023-05-07 04:03:00 -0300

    Minimmum working

---
+++ hybrid/commits/_commit_single.py
@@ -0,0 +1,26 @@
+def commit_single(self, move):
+    nurse = move["n"]
+    day = move["d"]
+    oldShift = self.helperVariables.projectedX[nurse][day]
+    newShift = move["s"]
+
+    
+    self.nurseModel.model.x[nurse][day][oldShift].lb = 0
+    self.nurseModel.model.x[nurse][day][oldShift].ub = 0
+    self.nurseModel.model.x[nurse][day][newShift].lb = 1
+    self.nurseModel.model.x[nurse][day][newShift].ub = 1
+
+    self.helperVariables.projectedX[nurse][day] = newShift
+
+    self.helperVariables.shiftTypeCounter[nurse][oldShift] -= 1
+    self.helperVariables.shiftTypeCounter[nurse][newShift] += 1
+
+    self.helperVariables.workloadCounter[nurse] += (self.nurseModel.data.parameters.l_t[newShift] - self.nurseModel.data.parameters.l_t[oldShift])
+
+    self.penalties.numberNurses[day][oldShift] -= 1
+    self.penalties.numberNurses[day][newShift] += 1
+
+    self.penalties.demand = self.math_single_demand(day, oldShift, newShift)
+    self.penalties.preference_total = self.math_single_preference(nurse, day, oldShift, newShift)
+
+    self.penalties.total = self.penalties.demand + self.penalties.preference_total
